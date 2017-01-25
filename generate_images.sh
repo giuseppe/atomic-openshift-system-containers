9079b6e2cfa96abf7571720bb5136622d8eec580
Giuseppe Scrivano <gscrivan@redhat.com>
2017-01-25 12:18:06 +0100

    Support different name for MASTER_IMAGE
    Generating the origin images can be done as:
    
    SRC_PREFIX=openshift MASTER_IMAGE=origin SRC_TAG=v1.3.1 DEST_TAG=v1.3.1 SRC_REGISTRY=docker.io sudo -E ./generate_images.sh localhost:5000
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- generate_images.sh
+++ generate_images.sh
@@ -2,10 +2,11 @@
 set -euo pipefail
 
 SRC_REGISTRY=${SRC_REGISTRY-registry.access.redhat.com}
-SRC_PREFIX=openshift3
+SRC_PREFIX=${SRC_PREFIX-openshift3}
 
 SRC_TAG=${SRC_TAG-v3.3.0.26}
 DEST_TAG=${DEST_TAG-v3.3.0.26}
+MASTER_IMAGE=${MASTER_IMAGE-ose}
 NO_PULL=${NO_PULL-}
 
 if test $# -eq 0; then
@@ -19,7 +20,7 @@ fi
 DEST_PREFIX=$1/$SRC_PREFIX
 
 if test x$NO_PULL != x1; then
-    for i in ose node openvswitch; do
+    for i in $MASTER_IMAGE node openvswitch; do
         docker pull $SRC_REGISTRY/$SRC_PREFIX/$i:$SRC_TAG
     done
 fi
@@ -32,10 +33,12 @@ clean () {
 
 trap clean EXIT
 
-for i in ose node openvswitch; do
+for i in $MASTER_IMAGE node openvswitch; do
+    echo build $i
     rm -rf _build
     mkdir _build
-    cp $i/* _build
+    src=$(sed -e"s|$MASTER_IMAGE|ose|g" <<<$i)
+    cp $src/* _build
     sed -e "s|\$ORIGIN|$SRC_REGISTRY/$SRC_PREFIX/$i:$SRC_TAG|g" < _build/Dockerfile.in > _build/Dockerfile
     cat _build/Dockerfile
     docker build -t $DEST_PREFIX/$i:$DEST_TAG _build
@@ -43,6 +46,6 @@ done
 
 set +euo pipefail
 
-for i in ose node openvswitch; do
+for i in $MASTER_IMAGE node openvswitch; do
     yes | docker push $DEST_PREFIX/$i:$DEST_TAG
 done
