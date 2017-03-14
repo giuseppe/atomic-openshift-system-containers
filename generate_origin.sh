add82363acf78d8408b1fd9e767ec488fe6e1c46
Giuseppe Scrivano <gscrivan@redhat.com>
2017-03-14 12:51:57 +0100

    generate_origin.sh: by default use 1.4.1
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- generate_origin.sh
+++ generate_origin.sh
@@ -2,7 +2,7 @@
 export SRC_REGISTRY=docker.io
 export SRC_PREFIX=openshift
 export MASTER_IMAGE=origin
-export SRC_TAG=${SRC_TAG-v1.3.1}
-export DEST_TAG=${DEST_TAG-v1.3.1}
+export SRC_TAG=${SRC_TAG-v1.4.1}
+export DEST_TAG=${DEST_TAG-v1.4.1}
 
 ./generate_images.sh $@
